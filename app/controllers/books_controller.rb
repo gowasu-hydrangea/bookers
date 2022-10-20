72d284365b8dd2e79e4c3decb2372485bd15cbed
gowasu-hydrangea <ajisai6liked@gmail.com>
2022-10-20 09:35:40 +0000

    [Finish]3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,47 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def new
+  end
+  
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end    
+    
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+     if @book.save
+       flash[:notice] = "successfully"
+       redirect_to book_path(@book.id)
+     else
+      @books = Book.all
+      render :edit
+     end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to '/books'
   end
+  
   
   private
   def book_params
